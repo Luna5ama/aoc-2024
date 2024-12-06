c7963694cb33c36d18d6bbf5ae8b544aa1e67151
Luna <62033805+Luna5ama@users.noreply.github.com>
2024-12-05 20:07:28 -0500

    D2

---
+++ src/main/kotlin/D2.kt
@@ -0,0 +1,46 @@
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
+fun main() {
+    fun isSafe(row: List<Int>): Boolean {
+        val firstDiff = row[1] - row[0]
+        return row.asSequence()
+            .windowed(2, 1)
+            .map { it[1] - it[0] }
+            .all { it.sign == firstDiff.sign && it.absoluteValue in 1..3 }
+    }
+
+    run {
+        val input = readInput("D2.txt")
+        val safeCount = input.lineSequence()
+            .filter { it.isNotBlank() }
+            .map { line ->
+                line.splitToSequence(" ").map { it.toInt() }.toList()
+            }.count { row ->
+                isSafe(row)
+            }
+
+        println("Part 1:")
+        println(safeCount)
+    }
+
+    run {
+        val input = readInput("D2.txt")
+        val safeCount = input.lineSequence()
+            .filter { it.isNotBlank() }
+            .map { line ->
+                line.splitToSequence(" ").map { it.toInt() }.toList()
+            }
+            .count { inputRow ->
+                isSafe(inputRow) || inputRow.indices.any { index ->
+                    val newRow = mutableListOf<Int>()
+                    newRow.addAll(inputRow.subList(0, index))
+                    newRow.addAll(inputRow.subList(index + 1, inputRow.size))
+                    isSafe(newRow)
+                }
+            }
+
+        println("Part 2:")
+        println(safeCount)
+    }
+}
